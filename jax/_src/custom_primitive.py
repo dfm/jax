ff0901761b5cfecc6c084052ef43b8411ecd00b6
Dan Foreman-Mackey <danfm@google.com>
2024-06-19 11:12:24 -0400

    batching

--- jax/_src/custom_primitive.py
+++ jax/_src/custom_primitive.py
@@ -5,12 +5,15 @@ from jax._src import api_util
 from jax._src import core
 from jax._src import linear_util as lu
 from jax._src.interpreters import ad
+from jax._src.interpreters import batching
 from jax._src.interpreters import mlir
 from jax._src.interpreters import partial_eval as pe
+from jax._src.interpreters.batching import not_mapped
 from jax._src.tree_util import tree_flatten, tree_unflatten
-from jax._src.util import safe_map, split_list
+from jax._src.util import moveaxis, safe_map, safe_zip, split_list
 
 map = safe_map
+zip = safe_zip
 
 
 def build_custom_primitive(spec, *, name: str | None = None) -> Callable:
@@ -34,6 +37,8 @@ def build_custom_primitive(spec, *, name: str | None = None) -> Callable:
   prim.def_effectful_abstract_eval(custom_primitive_abstract_eval)
   mlir.register_lowering(prim, custom_primitive_lowering)
   ad.primitive_jvps[prim] = custom_primitive_jvp
+  ad.primitive_transposes[prim] = custom_primitive_transpose
+  batching.primitive_batchers[prim] = custom_primitive_batching
 
   return prim
 
@@ -92,10 +97,14 @@ def custom_primitive_jvp(primals, tangents, call_jaxpr: core.ClosedJaxpr,
   if hasattr(spec, "vjp_fwd"):
     assert hasattr(spec, "vjp_bwd")
     # TODO(dfm): Add vjp support
+    # The basic idea will be to return a dummy primitive wrapping vjp_bwd that
+    # only supports transposition. Look at custom_vjp for some inspiration, but
+    # the approach is different.
     raise NotImplementedError("todo support vjp")
 
   if not hasattr(spec, "jvp"):
-    raise NotImplementedError(f"'jvp' not implemented for custom primitive '{name}'")
+    raise NotImplementedError(
+        f"'jvp' not implemented for custom primitive '{name}'")
 
   _, primals = split_list(primals, [num_consts])
   const_tangents, tangents = split_list(tangents, [num_consts])
@@ -111,3 +120,46 @@ def custom_primitive_jvp(primals, tangents, call_jaxpr: core.ClosedJaxpr,
   assert out_tree2 == out_tree, "todo error"
 
   return primals_out, tangents_out
+
+
+def custom_primitive_transpose(cts_in, *args, call_jaxpr: core.ClosedJaxpr,
+                               num_consts: int, in_tree, out_tree, name: str,
+                               spec, kwargs):
+  del call_jaxpr
+  if not hasattr(spec, "transpose"):
+    raise NotImplementedError(
+        f"'transpose' not implemented for custom primitive '{name}'")
+  _, args = split_list(args, [num_consts])
+  py_args = tree_unflatten(in_tree, args)
+  py_cts_in = tree_unflatten(out_tree, cts_in)
+  py_cts_out = spec.transpose(py_cts_in, *py_args, **kwargs)
+  cts_out, cts_out_tree = tree_flatten(py_cts_out, is_leaf=lambda x: x is None)
+  assert cts_out_tree == in_tree, "todo error"
+  return [None] * num_consts + cts_out
+
+
+def custom_primitive_batching(args, dims, *, call_jaxpr: core.ClosedJaxpr,
+                              num_consts: int, in_tree, out_tree, name: str,
+                              spec, kwargs):
+  del call_jaxpr
+  if not hasattr(spec, "vmap"):
+    raise NotImplementedError(
+        f"'vmap' not implemented for custom primitive '{name}'")
+
+  _, args = split_list(args, [num_consts])
+  const_dims, dims = split_list(dims, [num_consts])
+  assert all(d is not_mapped for d in const_dims)
+
+  axis_size, = {x.shape[d] for x, d in zip(args, dims) if d is not not_mapped}
+  args = [x if d is not_mapped else moveaxis(x, d, 0) for x, d in zip(args, dims)]
+
+  py_args = tree_unflatten(in_tree, args)
+  args_batched = tree_unflatten(in_tree, [d is not not_mapped for d in dims])
+  py_out, out_batched = spec.vmap(axis_size, args_batched, *py_args, **kwargs)
+
+  out, out_tree1 = tree_flatten(py_out)
+  batched, out_tree2 = tree_flatten(out_batched)
+  assert out_tree1 == out_tree, "todo error"
+  assert out_tree2 == out_tree, "todo error"
+
+  return out, [0 if b else not_mapped for b in batched]
